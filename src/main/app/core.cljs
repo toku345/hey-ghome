d55730d90eff1133876e1f6d5af2f4697ef3ee53
Fumitaka Tokumitsu <toku345@gmail.com>
2018-12-09 19:28:52 +0900

    Initialize Commit

---
+++ src/main/app/core.cljs
@@ -0,0 +1,7 @@
+(ns app.core)
+
+(defn reload! []
+  (println "Code updated."))
+
+(defn main! []
+  (println "App loaded!"))
---
+++ src/test/app/core_test.cljs
@@ -0,0 +1,5 @@
+(ns app.core-test
+  (:require [cljs.test :refer [deftest is]]))
+
+(deftest sample-test
+  (is (= 2 (+ 1 1))))
