04cf44278d01dd1687c213b3740e47f6d276402e
Fumitaka Tokumitsu <toku345@gmail.com>
2018-12-16 17:48:42 +0900

    Add command `say`: use google-home-notifier

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -1,4 +1,15 @@
-(ns app.core)
+(ns app.core
+  (:require ["google-home-notifier" :as googlehome]))
+
+;; (defonce device-name "ghome")
+(defonce device-ip "192.168.3.11")
+(defonce language "ja")
+
+(defn say [text]
+  ;; (.device googlehome device-name "ja")
+  (.ip googlehome device-ip language)
+  (.notify googlehome text (clj->js (fn [res] (prn res)))))
+
 
 (defn reload! []
   (println "Code updated."))
