0fda3eba6413c6a4c6baaa767b50324cce08623b
Fumitaka Tokumitsu <toku345@gmail.com>
2018-12-16 21:43:26 +0900

    `say` from console

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -1,7 +1,20 @@
-(ns app.core)
+(ns app.core
+  (:require ["google-home-notifier" :as googlehome]))
+
+;; (defonce device-name "ghome")
+(defonce device-ip "192.168.3.11")
+(defonce language "ja")
+
+(defn say [text]
+  ;; (.device googlehome device-name "ja")
+  (.ip googlehome device-ip language)
+  (.notify googlehome text (clj->js (fn [res] (prn res)))))
 
 (defn reload! []
   (println "Code updated."))
 
-(defn main! []
+(defn dev-main []
   (println "App loaded!"))
+
+(defn main! [& args]
+  (say (first args)))
