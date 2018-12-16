c2d156d5fc3e2f99504ff420af8eb43ea074e455
Fumitaka Tokumitsu <toku345@gmail.com>
2018-12-16 22:12:00 +0900

    `say` repetedly (#5)

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -1,8 +1,9 @@
 (ns app.core
-  (:require ["google-home-notifier" :as googlehome]))
+  (:require ["google-home-notifier" :as googlehome]
+            ["readline" :as readline]))
 
 ;; (defonce device-name "ghome")
-(defonce device-ip "192.168.3.11")
+(defonce device-ip "192.168.3.11") ; TODO: change your Google Home's ip address
 (defonce language "ja")
 
 (defn say [text]
@@ -10,6 +11,10 @@
   (.ip googlehome device-ip language)
   (.notify googlehome text (clj->js (fn [res] (prn res)))))
 
+(defn watch-input []
+  (let [reader (.createInterface readline #js {:input js/process.stdin :output js/process.stdout})]
+    (.on reader "line" (fn [line] (say line)))
+    (.on reader "close" (fn [] nil))))
 
 (defn reload! []
   (println "Code updated."))
@@ -18,4 +23,6 @@
   (println "App loaded!"))
 
 (defn main! [& args]
-  (say (first args)))
+  (if (empty? args)
+    (watch-input)
+    (say (first args))))
